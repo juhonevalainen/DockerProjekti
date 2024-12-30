875dba45c26e6a633893e9253be759fdb02e274a
juhonevalainen <juho21007@student.hamk.fi>
2024-12-31 00:27:11 +0200

    Parannuksia palvelimen sivujen pyyntöihin.

--- backend/server.py
+++ backend/server.py
@@ -1,4 +1,4 @@
-from flask import Flask, redirect, url_for, request, render_template
+from flask import Flask, request, render_template
 from flask_cors import CORS
 import json
 
@@ -13,7 +13,7 @@ def newMessage():
         saveMessage(str(received_message))
         return "<p>Vastaanotettu.</p>"
     else:
-        return "<p>Ei vastaanotettu.</p>"
+        return render_template('404.html'), 404
 
 # Tallennetaan vastaanotettu viesti JSON muodossa.
 def saveMessage(message):
@@ -27,18 +27,24 @@ def saveMessage(message):
 # Palautetaan kaikki tallennetut viestit.
 @app.route("/findmessage", methods=["GET"])
 def findMessage():
-    f = open("./data/messages.json")
-    data = json.load(f)
-    f.close()
-    return data
+    if request.method == "GET":
+        f = open("./data/messages.json")
+        data = json.load(f)
+        f.close()
+        return data
+    else:
+        return render_template('404.html'), 404
 
 # Poistetaan kaikki viestit.
 @app.route("/removemessage", methods=["POST"])
 def removemessage():
-    cleanMessages = {"allMessages": []}
-    with open("./data/messages.json", "w") as f:
-        json.dump(cleanMessages, f)
-    return "Viestit poistettu."
+    if request.method == "POST":
+        cleanMessages = {"allMessages": []}
+        with open("./data/messages.json", "w") as f:
+            json.dump(cleanMessages, f)
+        return "Viestit poistettu."
+    else:
+        return render_template('404.html'), 404
 
 @app.errorhandler(404)
 def page_not_found(error):
